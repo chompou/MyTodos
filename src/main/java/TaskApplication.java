b25f25b76646146c7b2758e24e025007aed50640
Jon Martin Kristiansen <jonmkr@stud.ntnu.no>
2021-03-17 16:52:00 +0100

    Added JavaFX dependencies and TaskApplication.java with Launcher.java for rudimentary prototyping

---
+++ src/main/java/TaskApplication.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+
+
+public class TaskApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
