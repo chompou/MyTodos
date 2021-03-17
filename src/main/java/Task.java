4549d685b15ea063e958f9ff97447aeb1cbb5d5d
Tobias Giverholt <tobgiver@gmail.com>
2021-03-17 14:30:18 +0100

    Created a very simple basis for this class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,35 @@
+import java.time.LocalDate;
+
 public class Task {
+    private String description;
+    private String category;
+    private LocalDate deadline;
+    private int priority;
+    private int status;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Task(String description, String category, LocalDate deadline, int priority, int status, LocalDate startDate, LocalDate endDate) {
+        this.description = description;
+        this.category = category;
+        this.deadline = deadline;
+        this.priority = priority;
+        this.status = status;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Task {" +
+                " description = '" + description + '\'' +
+                ", category='" + category + '\'' +
+                ", deadline=" + deadline +
+                ", priority=" + priority +
+                '}';
+    }
 }
