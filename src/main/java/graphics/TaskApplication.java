5753e64eaf81f8c4c0fed393cccd0d1f7dce2d1a
Jon Martin Kristiansen <jonmkr@stud.ntnu.no>
2021-04-08 12:26:59 +0200

    Added FXML files for main and task editing windows

--- src/main/java/graphics/TaskApplication.java
+++ src/main/java/graphics/TaskApplication.java
@@ -1,12 +1,25 @@
 package graphics;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import mytodos.TaskRegistry;
 
 
 public class TaskApplication extends Application {
+    TaskRegistry taskRegistry;
+
+
     @Override
     public void start(Stage stage) throws Exception {
+        taskRegistry = new TaskRegistry();
+
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/mytodomain.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 600 ,400);
+        stage.setScene(scene);
         stage.show();
     }
 
